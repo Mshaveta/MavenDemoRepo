0f4f87383d98909f9b6cd97df9a38ea76899900d
Mshaveta <Shaveta.malhotra47@gmail.com>
2019-03-13 01:06:20 +0530

    Update TestScript.java

--- src/test/java/TestScript.java
+++ src/test/java/TestScript.java
@@ -3,6 +3,6 @@ import org.testng.annotations.Test;
 public class TestScript {
 	@Test
 	public void m1() {
-		System.out.println("Method1");
+		System.out.println("Method1.....changes..............");
 	}
 }
